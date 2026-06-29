8288ca30fc228571022831cb5a8dc94bf89d8b94
radegundo <rade.salvador@gmail.com>
2026-06-29 02:14:14 +0200

    Somehow it works??

--- src/systems.rs
+++ src/systems.rs
@@ -2,7 +2,7 @@ use bevy::{ prelude::*, window::PrimaryWindow };
 use crate::grid::*;
 
 impl Grid {
-    fn new(size: GridSize) -> Self {
+    pub fn new(size: GridSize) -> Self {
         let (x, y) = (size.x, size.y);
         Self {
             size,
@@ -10,17 +10,14 @@ impl Grid {
             data: vec![vec![0; x as usize]; y as usize],
         }
     }
-    fn build(&mut self, window_query: Query<&Window, With<PrimaryWindow>>) {
-        if let Ok(window) = window_query.single() {
-            let window_width = window.width();
-            let window_height = window.height();
-            let (x, y) = (self.size.x, self.size.y);
-            self.cell_data = Cell {
-                size: Vec2::new(window_width / (x as f32), window_height / (y as f32)),
-            };
-        }
+    pub fn build(&mut self, window_size: Vec2<>) {
+        let (window_width, window_height) = (window_size[0], window_size[1]);
+        let (x, y) = (self.size.x, self.size.y);
+        self.cell_data = Cell {
+            size: Vec2::new(window_width / (x as f32), window_height / (y as f32)),
+        };
     }
-    fn draw(&self, gizmos: &mut Gizmos) {
+    pub fn draw(&self, gizmos: &mut Gizmos) {
         let (cell_x, cell_y) = (self.cell_data.size.x, self.cell_data.size.y);
         for i in 0..self.size.x {
             for j in 0..self.size.y {
